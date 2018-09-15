bdb7e2a0c1a41a1400db8fc9badec5423699ea5b
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-09-15 09:08:36 +0700

    Fixed msg bug

--- antivirus.bat
+++ antivirus.bat
@@ -1,3 +1,4 @@
 @echo off
 color 0a
-msg Jet Antivirus 0.1. This will remove any viruses that are present on your computer. This is open-source.
+@echo Jet Antivirus 0.1. This will remove any viruses that are present on your computer. This is open-source.
+pause
