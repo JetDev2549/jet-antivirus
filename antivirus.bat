59f6d29b37da23d6433882b62e55bafeb050e75d
JetDev2719 <34917635+JetDev2549@users.noreply.github.com>
2018-09-15 09:26:58 +0700

    Added yes no question

--- antivirus.bat
+++ antivirus.bat
@@ -1,4 +1,15 @@
 @echo off
 color 0a
-@echo Jet Antivirus 0.1. This will remove any viruses that are present on your computer. This is open-source.
+@echo Jet Antivirus 0.1 Beta 3. This will remove any viruses that are present on your computer. This is open-source.
+:choice
+set /P c=Do you want to scan your computer now? [Y/N]
+if /I "%c%" EQU "Y" goto :scan
+if /I "%c%" EQU "N" goto :noscan
+goto :choice
+
+:scan
+@echo Scanning
+
+:noscan
+@echo You may close this program.
 pause
